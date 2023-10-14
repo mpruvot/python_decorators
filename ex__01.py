c8c089948936b65ba2983b78560c193d2269b4c9
mpruvot <132161864+mpruvot@users.noreply.github.com>
2023-10-15 00:56:15 +0200

    add inner decorator

--- ex__01.py
+++ ex__01.py
@@ -1,4 +1,5 @@
 import logging
+from typing import Any
 from functools import wraps
 
 logging.basicConfig(
@@ -15,21 +16,30 @@ class Player:
         self.pv = pv
         self.lvl = lvl
 
-def log_decorator(func):
-    def wrapper(*args, **kwargs):
-        logging.info(f"calling function {func.__name__} with args {args} and kwargs {kwargs}")
-        res = func(*args, **kwargs)
-        logging.info(f"function {func.__name__} return {res}")
-    return wrapper
+def log_decorator(log_return: bool):
+    def inner_log_decorator(func):
+        def wrapper(*args, **kwargs):
+            logging.info(f"calling function {func.__name__} with args {args} and kwargs {kwargs}")
+            res = func(*args, **kwargs)
+            if log_return == True:
+                logging.info(f"function {func.__name__} return {res}")
+            return res
+        return wrapper
+    return inner_log_decorator
 
-@log_decorator
+@log_decorator(log_return=True)
 def new_player(pv: int, lvl: int, name: str):
     print(f"Player {name} is lvl {lvl} and has {pv} lp !")
     return Player(name, pv, lvl)
 
 
-new_player(100, 20, name="Marius")
+
+if __name__ == "__main__":
+    marius = new_player(100, 20, name="Marius")
+    print(marius.name, marius.pv, marius.lvl)    
+
 
 
 #https://www.tutorialspoint.com/How-to-get-a-function-name-as-a-string-in-Python#:~:text=The%20first%20approach%20is%20by,when%20the%20property%20is%20called.
 #https://www.youtube.com/watch?v=4jBJhCaNrWU
+#https://blog.miguelgrinberg.com/post/the-ultimate-guide-to-python-decorators-part-iii-decorators-with-arguments
